88ec1209646e8afa23a8da8ffea84cc93be595e9
fneema <fbenson@aimsammi.org>
2020-01-18 01:13:05 +0000

    updatedmodel

--- model.py
+++ model.py
@@ -7,8 +7,10 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.n_feature = n_feature
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_feature, kernel_size=5)
-        self.conv2 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
-        self.fc1 = nn.Linear(n_feature*53*53, 50)
+        self.conv2 = nn.Conv2d(n_feature, 2*n_feature, kernel_size=5)
+        self.conv3 = nn.Conv2d(2*n_feature, 2**2*n_feature, kernel_size=5)
+        self.conv4 = nn.Conv2d(2**2*n_feature, n_feature, kernel_size=5)
+        self.fc1 = nn.Linear(n_feature*10*10, 50)
         self.fc2 = nn.Linear(50, 2)
         
     def forward(self, x, verbose=False):
@@ -18,7 +20,13 @@ class CNN(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, kernel_size=2)
-        x = x.view(-1, self.n_feature*53*53)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = x.view(-1, self.n_feature*10*10)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
